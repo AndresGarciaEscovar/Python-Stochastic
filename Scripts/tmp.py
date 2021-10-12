7d7eb261e12a9e20e87eb8f6ba10fefe853eaa44
Andres Garcia <andrumen1@yahoo.com>
2021-10-11 22:42:28 -0500

    ...

--- Scripts/tmp.py
+++ Scripts/tmp.py
@@ -1,3 +1,21 @@
-import stochastic.RSA.rsa as rsa
+import copy as cp
+from stochastic.RSA.rsa import RSA, RSADimer, RSAParameters, RSAResultsAnalysis
 
-rsa.print_func()
+parameters = RSAParameters()
+
+# Set the length.
+parameters.length = 50
+
+# Set the maximum time.
+parameters.maximum_time = 6.0
+
+# Set the number of repetitions.
+parameters.repetitions = 1007
+
+a = RSA(parameters)
+
+a.run_simulation()
+
+a.print_results("final_results.txt")
+
+RSAResultsAnalysis.plot_lattices("lattice.txt")
