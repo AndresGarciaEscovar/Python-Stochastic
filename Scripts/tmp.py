590515c3c9b972d801e21b1e855baa8c1640eb36
Andres Garcia <andrumen1@yahoo.com>
2021-11-06 15:02:54 -0500

    Reformatted in a better way. Must finish the interface and the migration.

--- Scripts/tmp.py
+++ Scripts/tmp.py
@@ -1,5 +1,8 @@
 import copy as cp
-from stochastic.RSA1D.rsa import RSA, RSADimer, RSAParameters, RSAResultsAnalysis
+from stochastic.Classes.RSA_parameters import RSAParameters
+from stochastic.Interfaces.RSA_interface import RSA
+
+from stochastic.Classes.RSA_analysis import RSAResultsAnalysis
 
 parameters = RSAParameters()
 
@@ -11,11 +14,11 @@ parameters.maximum_time = 6.0
 
 # Set the number of repetitions.
 parameters.repetitions = 1007
+parameters.periodic = True
+parameters.tolerance = 1.0*10**(-5)
 
-a = RSA(parameters)
-
-a.run_simulation()
-
-a.print_results("final_results.txt")
-
+simulation = RSA(parameters)
+simulation.run_simulation()
 RSAResultsAnalysis.plot_lattices("lattice.txt")
+
+print(simulation.validate_adsorb(0))
---
+++ stochastic/Classes/RSA_parameters.py
@@ -0,0 +1,41 @@
+""" File that contains the RSAParameters class."""
+
+
+# ------------------------------------------------------------------------------
+# Imports.
+# ------------------------------------------------------------------------------
+
+# Imports: General.
+import time
+
+from dataclasses import dataclass
+
+# ------------------------------------------------------------------------------
+# Classes.
+# ------------------------------------------------------------------------------
+
+
+@dataclass
+class RSAParameters:
+    """ A data class where the parameters of the system are defined."""
+
+    # Lattice save file path.
+    lattice_file: str = "lattice.txt"
+
+    # Defines the length of the lattice.
+    length: int = 1
+
+    # Define the seed with which the generator must be seeded (i.e., the system time by default).
+    seed: int = int(time.time())
+
+    # Define the maximum simulation time.
+    maximum_time: float = 0.1
+
+    # Flags if the lattice is periodic.
+    periodic: bool = False
+
+    # Define the number of times the simulation must be executed.
+    repetitions: int = 1000
+
+    # Defines the tolerance, i.e., how close two numbers must be, to be equal.
+    tolerance = 1.0*10**(-5)
