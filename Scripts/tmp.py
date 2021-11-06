d878b6395d3cc1a19591ed59b4a4149b91f5df8b
andres <andrumen1@yahoo.com>
2021-11-06 16:30:59 -0500

    Gave it a better structure.

--- Scripts/tmp.py
+++ Scripts/tmp.py
@@ -3,21 +3,19 @@ from stochastic.Classes.dimers import Dimers
 from stochastic.Utilities.RSA_analysis import RSAResultsAnalysis
 from stochastic.Utilities.RSA_parameters import RSAParameters
 
+if __name__ == "__main__":
+    # Set the parameters.
+    parameters = RSAParameters()
+    parameters.length = 25
+    parameters.maximum_time = 6.0
+    parameters.repetitions = 1007
+    parameters.periodic = True
+    parameters.tolerance = 1.0*10**(-5)
 
-parameters = RSAParameters()
+    # Run the simulation.
+    simulation = Dimers(parameters)
+    simulation.run_simulation()
 
-# Set the length.
-parameters.length = 25
-
-# Set the maximum time.
-parameters.maximum_time = 6.0
-
-# Set the number of repetitions.
-parameters.repetitions = 1007
-parameters.periodic = True
-parameters.tolerance = 1.0*10**(-5)
-
-simulation = Dimers(parameters)
-simulation.run_simulation()
-RSAResultsAnalysis.plot_results("results.txt")
-RSAResultsAnalysis.plot_lattices("lattice.txt")
+    # Plot the graphs.
+    RSAResultsAnalysis.plot_results("results.txt")
+    RSAResultsAnalysis.plot_lattices("lattice.txt")
