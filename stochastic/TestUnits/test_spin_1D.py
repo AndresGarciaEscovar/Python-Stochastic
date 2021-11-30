33f9229e7f0518c726965f4afd144ca1ca98d2fb
Andres Garcia <andrumen1@yahoo.com>
2021-11-30 15:21:55 -0600

    Started to write the classes. Due to the similarity of the system might be worth to write less specific classes.

---
+++ stochastic/TestUnits/test_spin_1D.py
@@ -0,0 +1,30 @@
+""" File that contains the tests for the Spin1D class."""
+
+# ------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
+
+# Imports: General.
+import copy
+import itertools
+import numpy
+import random
+import unittest
+
+# Imports: User-defined classes.
+from stochastic.Classes.Spin1D.neighbors import Neighbors
+
+# ------------------------------------------------------------------------------
+# Classes.
+# ------------------------------------------------------------------------------
+
+
+class TestSpin1DNeighbors(unittest.TestCase):
+
+    def test_constructor(self):
+        self.assertTrue(True)
+
+
+# Executes the tests.
+if __name__ == '__main__':
+    unittest.main()
