f30bd4ac3a7e7679d75f6a75b2ebecb1aade13bb
andres <andrumen1@yahoo.com>
2026-01-23 22:51:33 -0600

    Finished fixing all the flake8 linting issues.

--- src/stochastic/classes/rsa_1d/dimers.py
+++ src/stochastic/classes/rsa_1d/dimers.py
@@ -1,17 +1,21 @@
-""" File that contains the random sequential adsorption of dimers.
+"""
+    File that contains the random sequential adsorption of dimers.
 """
 
-# ------------------------------------------------------------------------------
-# Imports.
-# ------------------------------------------------------------------------------
 
-# Imports: User-defined.
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Imports.
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+# User.
 from stochastic.interfaces.rsa_1d_interface import RSA1D
 from stochastic.utilities.rsa_parameters import RSA1DParameters
 
-# ------------------------------------------------------------------------------
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Classes.
-# ------------------------------------------------------------------------------
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
 class Dimers(RSA1D):
@@ -48,82 +52,83 @@ class Dimers(RSA1D):
 
         - self.tolerance: The tolerance to compare the floating point numbers.
     """
-
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    # /////////////////////////////////////////////////////////////////////////
     # Methods.
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-    # --------------------------------------------------------------------------
-    # Get Methods.
-    # --------------------------------------------------------------------------
+    # /////////////////////////////////////////////////////////////////////////
 
     def get_preheader(self) -> str:
-        """ Returns the pre-header, i.e., the string that contains the
+        """
+            Returns the pre-header, i.e., the string that contains the
             simulation information.
 
             :return: The string that represents the pre-header.
         """
-
         preheader = ",".join([
-            f"RSA Dimer", f"seed={self.seed}", f"repetitions(n)={self.repetitions}",
-            f"maximum time={self.maximum_time}", f"length={self.length}", f"periodic={self.periodic}"
+            "RSA Dimer",
+            f"seed={self.seed}",
+            f"repetitions(n)={self.repetitions}",
+            f"maximum time={self.maximum_time}",
+            f"length={self.length}",
+            f"periodic={self.periodic}"
         ])
 
         return preheader
 
-    # --------------------------------------------------------------------------
-    # Process Methods.
-    # --------------------------------------------------------------------------
-
     def process_adsorb(self) -> None:
-        """ Tries to perform an adsorption operation; i.e., select a random site
-            in the lattice onto which to adsorb.
+        """
+            Tries to perform an adsorption operation; i.e., select a random
+            site in the lattice onto which to adsorb.
         """
         generator = self.random_generator
-        site = generator.integers(0, self.length) if self.periodic else generator.integers(0, self.length - 1)
+        site = generator.integers(0, self.length)
+
+        if not self.periodic:
+            generator.integers(0, self.length - 1)
+
         self.attempts += 1
+
         if self.validate_adsorb(site):
             site_ = self.normalize_site(site + 1)
             self.lattice[site] = RSA1D.OCCUPIED
             self.lattice[site_] = RSA1D.OCCUPIED
             self.attempts_successful += 1
 
-    # --------------------------------------------------------------------------
-    # Validate Methods.
-    # --------------------------------------------------------------------------
-
     def validate_adsorb(self, site: int) -> bool:
-        """ Determines if the given site can adsorb a particle.
+        """
+            Determines if the given site can adsorb a particle.
 
             :param site: The site to be examined. Must be an integer number.
 
-            :return: If the site is empty and its inmediate neighbors are empty.
+            :return: If the site is empty and its inmediate neighbors are
+             empty.
         """
-
         site_ = self.normalize_site(site)
+
         if not self.lattice[site_] == RSA1D.EMPTY:
             return False
 
         examined = {site_}
-        if ((site + 1) >= self.length and self.periodic) or (site + 1) < self.length:
+
+        if (
+            ((site + 1) >= self.length and self.periodic) or
+            (site + 1) < self.length
+        ):
             site_ = self.normalize_site(site + 1)
-            if (site_ not in examined) and not self.lattice[site_] == RSA1D.EMPTY:
+
+            if not (site_ in examined or self.lattice[site_] == RSA1D.EMPTY):
                 return False
 
         return True
 
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    # Constructor and Dunder Methods.
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-    # --------------------------------------------------------------------------
+    # /////////////////////////////////////////////////////////////////////////
     # Constructor.
-    # --------------------------------------------------------------------------
+    # /////////////////////////////////////////////////////////////////////////
 
     def __init__(self, parameters: RSA1DParameters):
-        """ Initializes the simulation parameters.
+        """
+            Initializes the simulation parameters.
 
             :param parameters: A dataclass that contains the adjustable
              parameters of the simulation.
         """
-        super(Dimers, self).__init__(parameters)
+        super().__init__(parameters)
