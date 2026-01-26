a1d9806532b634def3bea5acd7b2adc50ef99912
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-01-26 06:31:28 -0600

    Updated to have the capabilities to print the default configuration.

--- src/stochastic/programs/rsa_1d_dimers/main.py
+++ src/stochastic/programs/rsa_1d_dimers/main.py
@@ -8,18 +8,39 @@
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
+# Standard library.
+from importlib.resources import files as ifiles
+
+
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Functions - Auxiliary
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
 # User.
+from stochastic.programs.rsa_1d_dimers import configs
 from stochastic.programs.rsa_1d_dimers.validation.parameters import validate
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Functions - Main
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+def print_parameters() -> None:
+    """
+        Prints the default parameters for the simulation.
+    """
+    # Auxiliary variables.
+    file: str = "parameters.json"
+    parameters: dict = {
+        "encoding": "utf-8",
+        "mode": "r"
+    }
+
+    # Open the file.
+    with ifiles(configs).joinpath(file).open(**parameters) as stream:
+        print(f"\n{stream.read()}", end="\n\n")
 
 
 def run(parameters: dict) -> dict:
