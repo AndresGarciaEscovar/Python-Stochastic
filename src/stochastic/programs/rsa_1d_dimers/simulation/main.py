f11699abd5aaa9104ddc08c4b6583d5c20928f8c
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-01-29 15:18:43 -0600

    Made a function to run the simulation.

--- src/stochastic/programs/rsa_1d_dimers/simulation/main.py
+++ src/stochastic/programs/rsa_1d_dimers/simulation/main.py
@@ -21,6 +21,27 @@ from stochastic.programs.rsa_1d_dimers.simulation.classes.statistics import (
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Functions - Auxiliary
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+def _run_simulation(parameters: dict) -> RSA1DDimersResults:
+    """
+        Runs the one time simulation the number of requested times.
+
+        :param parameters: The parameters to run the simulations.
+
+        :return: The processed results of the simulation.
+    """
+    attempts: int = parameters["simulation"]["attempts"]
+
+    statistics: RSA1DDimersStatistics = RSA1DDimersStatistics(parameters)
+    lattice: RSA1DDimersLattice = RSA1DDimersLattice(parameters)
+    results: RSA1DDimersResults = RSA1DDimersResults(parameters)
+
+    print(f"{results}")
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Main Function
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
@@ -34,8 +55,8 @@ def run(parameters: dict) -> RSA1DDimersResults:
         :return: The results of the simulation.
     """
     # Import the objects.
-    statistics: RSA1DDimersStatistics = RSA1DDimersStatistics(parameters)
-    lattice: RSA1DDimersLattice = RSA1DDimersLattice(parameters)
-    results: RSA1DDimersResults = RSA1DDimersResults(parameters)
+    results: RSA1DDimersResults = _run_simulation(parameters)
+
+    print("HERE")
 
     return results
