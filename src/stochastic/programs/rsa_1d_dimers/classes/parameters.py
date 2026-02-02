72b8e274ae422182dab177384582db653681282c
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-02-02 14:54:06 -0600

    Added string representations to the parameters.

--- src/stochastic/programs/rsa_1d_dimers/classes/parameters.py
+++ src/stochastic/programs/rsa_1d_dimers/classes/parameters.py
@@ -9,7 +9,7 @@
 
 
 # Standard library.
-import copy as cp
+from typing import Any
 
 # User.
 from stochastic.programs.rsa_1d_dimers.validation.parameters import validate
@@ -34,6 +34,46 @@ class Parameters:
         - self.simulation: A dictionary with the simulation parameters.
     """
     # /////////////////////////////////////////////////////////////////////////
+    # Methods - Dunder
+    # /////////////////////////////////////////////////////////////////////////
+
+    def __repr__(self) -> str:
+        """
+            Prints the current dictionary parameters in a reproducible way.
+        """
+        # Auxiliary variables.
+        parameters: dict = {
+            "history": self.history,
+            "output": self.output,
+            "simulation": self.simulation
+        }
+
+        return f"{parameters}"
+
+    def __str__(self) -> str:
+        """
+            Prints the current dictionary parameters in a friendly format.
+        """
+        indent: str = " " * 4
+        string: str = "{\n"
+        entries: tuple = (
+            ("history", self.history),
+            ("output", self.output),
+            ("simulation", self.simulation)
+        )
+
+        for key, dictionary in entries:
+            string = f"{string}{indent}'{key}': " + "{\n"
+
+            for subkey, value in dictionary.items():
+                value: Any = f"'{value}'" if isinstance(value, str) else value
+                string = f"{string}{indent * 2}'{subkey}': {value},\n"
+
+            string = f"{string}{indent}" + "},\n"
+
+        return f"{string}" + "}"
+
+    # /////////////////////////////////////////////////////////////////////////
     # Constructor
     # /////////////////////////////////////////////////////////////////////////
 
@@ -48,3 +88,5 @@ class Parameters:
         self.history: dict = final["history"]
         self.output: dict = final["output"]
         self.simulation: dict = final["simulation"]
+
+        print(f"{self}")
