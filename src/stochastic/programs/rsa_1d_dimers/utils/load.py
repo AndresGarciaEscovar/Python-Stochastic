3ab5d90cb236e157eca93d74ea63956819dcff3c
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-02-03 22:45:00 -0600

    Improved the spacing.

--- src/stochastic/programs/rsa_1d_dimers/utils/load.py
+++ src/stochastic/programs/rsa_1d_dimers/utils/load.py
@@ -11,6 +11,7 @@
 # User.
 from stochastic.programs.rsa_1d_dimers.simulation import Simulation
 
+
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Functions - Auxiliary
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
