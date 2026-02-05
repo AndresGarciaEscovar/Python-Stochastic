bf76188525ace4b7fef127546d454f086e6aa4da
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-02-05 14:02:34 -0600

    Started adding the functions to validate the loaded simulation.

--- src/stochastic/programs/rsa_1d_dimers/validation/load.py
+++ src/stochastic/programs/rsa_1d_dimers/validation/load.py
@@ -7,6 +7,15 @@
 # Imports
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
+
+# Standard library.
+from datetime import datetime
+
+# User.
+from stochastic.programs.rsa_1d_dimers.simulation import PROGRAM
+from stochastic.utilities.validate import validate_dictionary
+
+
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Global Variables
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -15,13 +24,8 @@
 # Base dictionary with which to compare.
 BASE: dict = {
     "_metadata": {
-        "name": "RSA 1D Dimers",
-        "save_date": "YYYYMMDDhhmmss"
-    },
-    "lattice": {
-        "lattice": [],
-        "length": 0,
-        "periodic": False
+        "name": PROGRAM,
+        "save_date": "%Y%m%d%H%M%S"
     },
     "parameters": {
         "history": {
@@ -49,6 +53,11 @@ BASE: dict = {
         "empty_double": [],
         "empty_triple": []
     },
+    "lattice": {
+        "lattice": [],
+        "length": 0,
+        "periodic": False
+    },
     "statistics": {
         "attempts": [],
         "coverage": [],
@@ -59,6 +68,9 @@ BASE: dict = {
         "periodic": False
     }
 }
+
+# Possible keys.
+REQUIRED_KEYS: tuple = tuple(BASE.keys())
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -72,6 +84,83 @@ def _validate_form(dictionary: dict) -> None:
 
         :param dictionary: The dictionary to be validated.
     """
+    # Validate the different quantities.
+    _validate_form_keys(dictionary)
+    _validate_form_metadata(dictionary["_metadata"])
+
+
+def _validate_form_keys(dictionary: dict) -> None:
+    """
+        Validates that the dictionary has the proper base keys.
+
+        :param dictionary: The dictionary to be validated.
+
+        :raise KeyError: If the keys of the dictionary do not match the
+         required keys.
+    """
+    # Auxiliary variables.
+    current: set = set(dictionary.keys())
+    expected_0: set =set(REQUIRED_KEYS)
+    expected_1: set =set(REQUIRED_KEYS[:-2])
+
+    # Validate the keys.
+    if current not in (expected_0, expected_1):
+        raise KeyError(
+            f"The key of the loaded dictionary do not match the required "
+            f"keys. Current keys: {current or '{}'}, expected keys: "
+            f"{expected_0 or '{}'} or {expected_1 or '{}'}."
+        )
+
+
+def _validate_form_metadata(dictionary: dict) -> None:
+    """
+        Validates that the metadata is properly set.
+
+        :param dictionary: The dictionary to be validated.
+
+        :param ValueError: If any of the values are not properly set.
+    """
+    # Auxiliary variables.
+    metadata: dict = BASE["_metadata"]
+
+    # Keys and types must match.
+    validate_dictionary(metadata, dictionary, error=True)
+
+
+def _validate_values(dictionary: dict) -> None:
+    """
+        Validates the values in the dictionary.
+
+        :param dictionary: The dictionary to be validated.
+    """
+    # Validate the values.
+    _validate_values_metadata(dictionary["_metadata"])
+
+
+def _validate_values_metadata(dictionary: dict) -> None:
+    """
+        Validates that the metadata is properly set.
+
+        :param dictionary: The dictionary to be validated.
+
+        :param ValueError: If any of the values are not properly set.
+    """
+    # Auxiliary variables.
+    metadata: dict = BASE["_metadata"]
+
+    # The names must match.
+    key: str = "name"
+
+    if dictionary[key] != metadata[key]:
+        raise ValueError(
+            f"The metadata \"{key}\" entry does not match the required value. "
+            f"Required value: \"{metadata[key]}\", current value: "
+            f"\"{dictionary[key]}\"."
+        )
+
+    # The date must be valid and have the proper format.
+    key = "save_date"
+    datetime.strptime(dictionary[key], metadata[key])
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -87,3 +176,4 @@ def validate_parameters(loaded: dict) -> None:
     """
     # Validate the dictionary.
     _validate_form(loaded)
+    _validate_values(loaded)
